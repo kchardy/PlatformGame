c9da5eeb596be2a1617d4fb0ee9a01b3b6345c8c
relena <k.chardy@o2.pl>
2018-07-05 22:38:41 +0200

    loop added

--- src/com/kchardy/game/Game.java
+++ src/com/kchardy/game/Game.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Game extends Canvas implements Runnable{
 
     public static final int WIDTH = 270;
-    public static final int HEIGH = WIDTH/14*10;
+    public static final int HEIGHT = WIDTH/14*10;
     public static final int SCALE = 4;
     public static final String TITLE = "Mario kiepska kopia";
 
@@ -17,21 +17,43 @@ public class Game extends Canvas implements Runnable{
     {
         if(running) return;
         running = true;
-        thread = new Thread();
+        thread = new Thread(this, "Thread");
+        thread.start();
     }
 
     private synchronized void stop()
     {
+        if(!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void run() {
+        while(running)
+        {
+            render();
+            update();
+        }
+    }
+
+    public void render()
+    {
+
+    }
+
+    public void update() //tick()
+    {
 
     }
     public Game()
     {
-        Dimension size = new Dimension(WIDTH*SCALE, HEIGH*SCALE);
+        Dimension size = new Dimension(WIDTH*SCALE, HEIGHT *SCALE);
         setPreferredSize(size);
         setMaximumSize(size);
         setMinimumSize(size);
